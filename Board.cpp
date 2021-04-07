81f2e2011e1381eebead54fd151b9d808f3a7455
odeyaMolodic <57504045+odeyaMolodic@users.noreply.github.com>
2021-04-07 19:26:40 +0300

    part b

--- Board.cpp
+++ Board.cpp
@@ -1,8 +1,10 @@
 /**
- * 
+ * message-board exercise
+ * This program allows you to read or write ads on the board
+ * And display it conveniently.
  * 
  * Author: Odeya Lavi
- * Since : 2021-03
+ * Since : 2021-04
  */
 
 #include "Board.hpp"
@@ -13,16 +15,60 @@ using namespace std;
 
 namespace ariel{
 
-    void Board::post(int row, int column, Direction direction, string message){
-        
+    void Board::resize(const unsigned int &row, const unsigned int &endRow, const unsigned int &column, const unsigned int &endCol, const unsigned int &length){
+        minRows = min(minRows, row);
+        maxRows = max(maxRows, endRow);
+        minColumns = min(minColumns, column);
+        maxColumns = max(maxColumns, endCol);
     }
 
-    string Board::read(int row, int column, Direction direction, int length){
-        return "";
+    void Board::post(unsigned int row, unsigned int column, Direction direction, const string &message){
+        unsigned int msgSize = message.size();
+        unsigned int endRow;
+        unsigned int endCol;
+        
+        if(direction == Direction::Vertical){
+            for(unsigned int i = 0; i < msgSize; i++){
+                board[row+i][column].letter = message.at(i);
+            }
+            endRow = row + msgSize-1;
+            endCol = column;
+
+        }else{
+            for(unsigned int i = 0; i < msgSize; i++){
+                board[row][column+i].letter = message.at(i);
+            }
+            endRow = row;
+            endCol = column + msgSize-1;
+        }
+
+        resize(row, endRow, column, endCol, msgSize);
+    }
+
+    string Board::read(unsigned int row, unsigned int column, Direction direction, unsigned int length){
+        string ans;
+        if(direction == Direction::Vertical){
+            for(unsigned int i = 0; i < length; i++){
+                ans += board[row+i][column].letter;
+            }
+        }else{
+            for(unsigned int i = 0; i < length; i++){
+                ans += board[row][column+i].letter;
+            }
+        }
+        return ans;
     }
 
     void Board::show(){
-        
+        //string spaces;
+        for (unsigned int i = minRows; i <= maxRows; i++) {
+            cout << i << ": ";
+            for (unsigned int j = minColumns; j <= maxColumns; j++) {
+                cout << board[i][j].letter;
+            }
+            cout << endl;
+        }
+        cout << endl;
     }
 
 }
--- Board.hpp
+++ Board.hpp
@@ -1,22 +1,47 @@
 #include "Direction.hpp"
+#include <limits.h>
 #include <string>
+#include <map>
 using namespace std;
+
+struct blankChar{
+    char letter = '_';
+};
 
 namespace ariel{
 
     class Board {
 
+        map<unsigned int, map<unsigned int, blankChar>> board;
+        unsigned int minRows;
+        unsigned int maxRows;
+        unsigned int minColumns;
+        unsigned int maxColumns;
+
+        void resize(const unsigned int &row, const unsigned int &endRow, const unsigned int &column, const unsigned int &endCol, const unsigned int &length);
+
     public:
 
-        Board() { }
+        Board(): maxRows(0), maxColumns(0), minRows(UINT_MAX), minColumns(UINT_MAX) { }
 
         ~Board() { }
 
-        //unsigned?
-        void post(int row, int column, Direction direction, string message);
+        /*
+        Gets a position (row and column), direction (horizontal or vertical), and a string that represents an ad, 
+        and pastes the string on the board in the appropriate place.
+        */
+        void post(unsigned int row, unsigned int column, Direction direction, const string &message);
 
-        string read(int row, int column, Direction direction, int length);
+        /*
+        Gets position (row and column), direction (horizontal or vertical), and number of characters, 
+        reads what is written on the board at the given position and length, and returns a string. 
+        The letter in each place on the board is the last letter pasted there.
+        */
+        string read(unsigned int row, unsigned int column, Direction direction, unsigned int length);
 
+        /*
+        Displays the current message-board for easy reading
+        */
         void show();
     }; 
 
